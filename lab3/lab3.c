4f32ee550dc440636b228991c66233bcd6c1fd2f
Adam Tygart <adam.tygart@gmail.com>
2011-02-07 13:43:35 -0600

    pointer notation forloop in lab3

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void print(int* val);
+void print(int* val, int length);
 
 int main() {
 	int* arr = malloc(sizeof(int) * 10);
@@ -10,12 +10,14 @@ int main() {
 	int i;
 	for(i=1; i<=10; i++) {
 		arr[i-1] = i;
-		print(&i);
 	}
+	print(arr, 10);
 	free(arr);
 	return 0;
 }
 
-void print(int* val) {
-	printf("%d\n", *val);
+void print(int* val, int length) {
+	int* i;
+	for (i = val; i < val+length; i++)
+		printf("%d\n", *i);
 }
